1f90cae386e5c0578fc6a785fe6913ce68628f1e
Ryusei Hanado <hana87business@gmail.com>
2024-03-30 13:01:24 +0900

    V0.0.24

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <p>
-          V0.0.23
+          V0.0.24
         </p>
         <div>
           <a
