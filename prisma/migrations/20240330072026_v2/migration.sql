4170380f5a330569e0dc6c0819b8e7a536c521af
Ryusei Hanado <hana87business@gmail.com>
2024-03-30 16:30:42 +0900

    V0.0.37

---
+++ prisma/migrations/20240330072026_v2/migration.sql
@@ -0,0 +1,56 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `provider` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerAccountId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `scope` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `type` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the `PasswordResetToken` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `email` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "PasswordResetToken" DROP CONSTRAINT "PasswordResetToken_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "Account_provider_providerAccountId_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "provider",
+DROP COLUMN "providerAccountId",
+DROP COLUMN "refresh_token",
+DROP COLUMN "scope",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type",
+DROP COLUMN "type",
+ADD COLUMN     "email" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "PasswordResetToken";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "VerificationToken";
