6b79f91b23d0d86df192acff07bf551ac54bc580
Ryusei Hanado <hana87business@gmail.com>
2024-03-30 17:54:51 +0900

    V0.0.41: note機能を追加

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <p>
-          V0.0.40
+          V0.0.41
         </p>
         <div>
           <a
